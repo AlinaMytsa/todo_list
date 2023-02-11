f36a45fbb306af1294b46ab93cf8fded6fa010a3
AlinaMytsa <act.mitsa@gmail.com>
2023-02-11 20:05:20 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import FilterButton from "./components/FilterButton";
 import React, {useState} from "react";
 import {nanoid} from "nanoid";
 
+// Примітка: ми визначаємо ці константи поза нашою App()функцією, тому що якби вони були визначені всередині неї,
+// вони б перераховувалися щоразу, коли компонент <App />повторно візуалізується, а ми цього не хочемо.
+// Ця інформація ніколи не зміниться, що б не робила наша програма.
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 
 function App(props) {
-  const [tasks, setTasks] = useState(props.tasks)
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
 
 
   const addTask = (name) => {
@@ -25,12 +38,44 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
-  const taskList = tasks.map((task) =>
-    <Todo id={task.id}
-          name={task.name}
-          completed={task.completed}
-          key={task.id}
-          toggleTaskCompleted={toggleTaskCompleted}/>)
+  const deleteTask = (id) => {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const editTask = (id, newName) => {
+    const editedTaskList = tasks.map((task) => {
+      if (id === task.id) return {...task, name: newName}
+
+      return task
+    })
+    setTasks(editedTaskList)
+  }
+
+  const filterList = FILTER_NAMES.map((name) => (
+
+    <FilterButton key={name}
+                  name={name}
+                  isPressed={true}
+                  setFilter={setFilter}/>
+
+  ))
+
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+
 
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task'
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
@@ -44,9 +89,8 @@ function App(props) {
 
       <div className="filters btn-group stack-exception">
 
-        <FilterButton/>
-        <FilterButton/>
-        <FilterButton/>
+        {filterList}
+
 
       </div>
 
@@ -55,10 +99,7 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-
         {taskList}
-
-
       </ul>
     </div>
   );
